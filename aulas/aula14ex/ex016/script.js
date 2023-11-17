9b3dc648ec0d2e56d68eaa7c5466eac589db962b
Ayane Brígida <143612664+ayanebrigida@users.noreply.github.com>
2023-11-16 22:19:07 -0300

    aula14ex

--- aulas/aula14ex/ex016/script.js
+++ aulas/aula14ex/ex016/script.js
@@ -4,12 +4,28 @@ function contar() {
     var passo1 = document.getElementById('passo')
     var res = document.getElementById('res')
 
-    var inicio = Number(inicio1.value)
-    var fim = Number(fim1.value)
-    var passo = Number(passo1.value)
-
-    res.innerHTML = '<p>Contando:</p>'
-    for (var c = inicio; c <= fim; c = c + passo) {
-        res.innerHTML += `${c} 👉 `
+    if (inicio1.value.length == 0 || fim1.value.length == 0 || passo1.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+    } else  {  
+        res.innerHTML = 'Contando:<br>' 
+        var inicio = Number(inicio1.value)
+        var fim = Number(fim1.value)
+        var passo = Number(passo1.value)
+        if (passo <=0) {
+            window.alert('Passo inválido! Considerar PASSO 1')
+            passo = 1
+        }
+        if(inicio < fim) {
+            //Contagem crescente
+            for (var c = inicio; c <= fim; c += passo) {
+                res.innerHTML += `${c} 👉 `
+            }
+        } else {
+            //Contagem regressiva
+            for (var c = inicio; c >= fim; c -= passo) {
+            res.innerHTML += `${c} 👉 `
+            }
+        }
+        res.innerHTML += `\u{1F3C1} `
     }
 }
--- aulas/aula14ex/ex017/script.js
+++ aulas/aula14ex/ex017/script.js
@@ -1,10 +1,18 @@
 function gerar() {
     var nume = document.getElementById('num')
-    var num = Number(nume.value)
     var res = document.getElementById('res')
-
-    for (var c = 1; c = 10; c++) {
-        res.innerHTML = `${num} X ${c} = ${x}`
-        var x = num * c
+    if (nume.value.length == 0) {
+        window.alert('Por favor, digiteum número!')
+    } else {
+        var num = Number(nume.value)
+        res.innerHTML = ''
+        for (var c = 1; c <= 10; c++) {
+            var item = document.createElement('option')
+            item.text = `${num} X ${c} = ${num * c}`
+            item.value = `res${c}`
+            res.appendChild(item)
+        }
     }
-}
+    }
+
+    
