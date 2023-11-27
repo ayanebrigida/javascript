48db62b9708968853e105f50a2ee68f818906e0f
Ayane Brígida <143612664+ayanebrigida@users.noreply.github.com>
2023-11-27 10:48:14 -0300

    fimAula16ex

--- aulas/aula16ex/ex018/script.js
+++ aulas/aula16ex/ex018/script.js
@@ -39,12 +39,25 @@ function adicionar() {
         if (valores.length == 0) {
             window.alert('Adicione valores antes de finalizar')
         } else {
-            valores.sort()
-        res.innerHTML = `<p>Ao todo, temos ${valores.length} números cadastrados.</p>`
-        //res.innerHTML += `<p>O maior valor informado foi ${}.</p>`
-        res.innerHTML += `<p>O menor valor informado foi ${valores[0]}.</p>`
-        //res.innerHTML += `<p>Somando todos os valores, temos${x}.</p>`
-        //res.innerHTML += `<p>A média dos valores é ${x}.</p>`
+            let tot = valores.length
+            let maior = valores[0]
+            let menor = valores[0]
+            let soma = 0
+            let media = 0
+
+            for (let pos in valores) {
+                soma += valores[pos]
+                if (valores[pos] > maior) 
+                    maior = valores[pos]
+                if (valores[pos] < menor)
+                    menor = valores[pos]
+            } 
+            media = soma / tot
+        res.innerHTML = `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}.</p>`
         }
         
     }
